f916cab68524f22f8674c793b010e83a73cba585
Marie Groß <71376763+mariegross@users.noreply.github.com>
2021-02-04 19:04:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+![Node.js CI](https://github.com/mariegross/react-template/workflows/Node.js%20CI/badge.svg)
+
+# react-template
---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import ExButton from "./components/Button";
+
+function App() {
+  return (
+    <div className="App">
+      <ExButton />
+      <a href="/storybook">To Storybook</a>
+    </div>
+  );
+}
+
+export default App;
