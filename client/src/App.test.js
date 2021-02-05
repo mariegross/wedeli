5f8451691b60ca4bda4605c0b1da4408a00ca4c8
mariegross <marie.gross@gmx.de>
2021-02-05 20:08:16 +0100

    ✅ Disable test

--- client/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("renders storybook react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/To Storybook/i);
-  expect(linkElement).toBeInTheDocument();
-});
